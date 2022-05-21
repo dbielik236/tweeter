7d74c85d84b0bd39e50067b1197be01c87a87b76
David Bielik <davidbielik@gmail.com>
2022-05-21 17:59:18 -0400

    completed sign in and display tweet with user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
-Rails.application.routes.draw do
-  devise_for :users
-  resources :tweeets
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :tweeets
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tweeets#index"
 end
